992e5f54f4093ebb6509c1507ca9cbb7b8d56d43
Kieran Parikh <kieran@parikh.ca>
2023-08-09 18:57:14 -0400

    pixel first attempt

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,84 +1,84 @@
-const path = require("path")
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const glob = require('glob')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
 const fs = require("fs");
 
 function determineDateForFile(path) {
-    const regex = new RegExp('(?<=Date:).*')
-    const fileTextA = fs.readFileSync(path, 'utf8')
-    const matchesA = regex.exec(fileTextA)
+    const regex = new RegExp("(?<=Date:).*");
+    const fileTextA = fs.readFileSync(path, "utf8");
+    const matchesA = regex.exec(fileTextA);
     if (matchesA != null) {
-        return new Date(Date.parse(matchesA[0]))
+        return new Date(Date.parse(matchesA[0]));
     } else {
         const statsObjA = fs.statSync(path);
-        return statsObjA.birthtime
+        return statsObjA.birthtime;
     }
 }
 
 function sortFn(a, b) {
-    const dateA = determineDateForFile(a)
-    const dateB = determineDateForFile(b)
+    const dateA = determineDateForFile(a);
+    const dateB = determineDateForFile(b);
 
     if (dateA.valueOf() === dateB.valueOf()) {
-        return 0
+        return 0;
     } else if (dateA.valueOf() > dateB.valueOf()) {
-        return 1
+        return 1;
     } else {
-        return -1
+        return -1;
     }
 }
 
 function generate_index() {
-    const regex = new RegExp('(?<=Description:).*')
-    return "<html><title>kieran's sketchbook</title><body><link rel=\"stylesheet\" href=\"./index.css\"><a class=\"backButton\" href='https://kieran.lol'>BACK</a><h1>KIERAN'S SKETCHBOOK</h1><br><br>" + glob.sync('./src/**.ts').sort(sortFn).reverse().reduce(function(obj, el){
-        const name = path.parse(el).name
-        const fileText = fs.readFileSync(el, 'utf8')
-        const matches =  regex.exec(fileText)
-        let description = "No description provided."
+    const regex = new RegExp("(?<=Description:).*");
+    return "<html><title>kieran's sketchbook</title><body><link rel=\"stylesheet\" href=\"./index.css\"><a class=\"backButton\" href='https://kieran.lol'>BACK</a><h1>KIERAN'S SKETCHBOOK</h1><br><br>" + glob.sync("./src/**.ts").sort(sortFn).reverse().reduce(function(obj, el){
+        const name = path.parse(el).name;
+        const fileText = fs.readFileSync(el, "utf8");
+        const matches =  regex.exec(fileText);
+        let description = "No description provided.";
         if (matches !== null && matches.length === 1) {
-             description =  matches[0]
+            description =  matches[0];
         }
-        return obj.concat(`<a href="${name}.html">${name}</a><p class="date">${determineDateForFile(el).toLocaleDateString()}</p><p class="description">${description}</p><br>`)
-    }, "") + "</body></html>"
+        return obj.concat(`<a href="${name}.html">${name}</a><p class="date">${determineDateForFile(el).toLocaleDateString()}</p><p class="description">${description}</p><br>`);
+    }, "") + "</body></html>";
 }
 
 module.exports = {
-  entry: glob.sync('./src/**.ts').reduce(function(obj, el){
-    obj[path.parse(el).name] = "./" + el;
-    return obj
-  },{}),
-  output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "[name].js",
-    clean: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+    entry: glob.sync("./src/**.ts").reduce(function(obj, el){
+        obj[path.parse(el).name] = "./" + el;
+        return obj;
+    },{}),
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].js",
+        clean: true
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/i,
-        exclude: /node_modules/,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: glob.sync('./src/**.ts').reduce(function(obj, el){
-    const name = path.parse(el).name
-    obj.push(
-        new HtmlWebpackPlugin({
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/i,
+                exclude: /node_modules/,
+                use: "@davcri/webpack-glsl-loader",
+            },
+            {
+                test: /\.ts?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: glob.sync("./src/**.ts").reduce(function(obj, el){
+        const name = path.parse(el).name;
+        obj.push(
+            new HtmlWebpackPlugin({
                 template: "./public/page.html",
                 templateParameters: {
                     title: name,
@@ -87,13 +87,13 @@ module.exports = {
                 publicPath: "./",
                 filename: `${name}.html`,
                 chunks: [`${name}`]
-              }))
-    return obj
-  },[
-      new HtmlWebpackPlugin({
-      templateContent: generate_index(),
-      filename: 'index.html',
-      chunks: [],
-      }),
-  ]),
-}
+            }));
+        return obj;
+    },[
+        new HtmlWebpackPlugin({
+            templateContent: generate_index(),
+            filename: "index.html",
+            chunks: [],
+        }),
+    ]),
+};
---
+++ src/pixel_01.ts
@@ -0,0 +1,95 @@
+import * as P5 from "p5";
+import {effect, stars, rect} from "./pixel/effects";
+
+
+const sketch = (s: P5) => {
+    const COLUMNS_AND_ROWS = 128;
+    let PIXEL_WIDTH = 10;
+    let t = 0;
+    const globalTimeMultiplier = 0.4;
+    const effects: effect[] = [
+        new stars(COLUMNS_AND_ROWS/2, COLUMNS_AND_ROWS/2, COLUMNS_AND_ROWS, COLUMNS_AND_ROWS, s.color("#ffffff")),
+        new rect(COLUMNS_AND_ROWS/2, COLUMNS_AND_ROWS/2, 0.75*COLUMNS_AND_ROWS, 0.75*COLUMNS_AND_ROWS, s.color("#13c4b4")),
+        // new circle(100, 100, 100, 20),
+        // new mountain(100, 150, 200, 200, ),
+        // new mountain(100, 150, 180, 200, s.color('#000000')),
+        // new mountain(100, 150, 160, 200, s.color('#ffffff')),
+        // new mountain(100, 150, 100, 200),
+    ];
+    let shader: P5.Shader = null;
+    let frameRateVisible = true;
+    let lastFrameRateEmission = 0;
+
+    s.setup = () => {
+        PIXEL_WIDTH = s.min( window.innerWidth/COLUMNS_AND_ROWS, window.innerHeight/COLUMNS_AND_ROWS);
+        s.createCanvas(PIXEL_WIDTH*COLUMNS_AND_ROWS, PIXEL_WIDTH*COLUMNS_AND_ROWS, s.WEBGL);
+        s.background(220);
+        s.noSmooth();
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        shader = s.createShader(require("./pixel/shaders/shader.vert"), require("./pixel/shaders/shader.frag"));
+        // s.pixelDensity(1)
+        s.noSmooth();
+    };
+
+    s.draw = () => {
+        s.noStroke();
+        s.background(255);
+
+        shader.setUniform("u_time", t);
+        shader.setUniform("u_resolution", [COLUMNS_AND_ROWS, COLUMNS_AND_ROWS]);
+
+        const types = [];
+        const centers = [];
+        const dimensions = [];
+        const colors = [];
+        const settings = [];
+
+        for (const effect of effects) {
+            effect.setEffectStateForTime(t);
+            types.push(effect.type);
+            centers.push(effect.x, effect.y);
+            dimensions.push(effect.width, effect.height);
+            if (effect.color !== null) {
+                colors.push(s.red(effect.color)/255, s.green(effect.color)/255, s.blue(effect.color)/255, s.alpha(effect.color)/255);
+            } else {
+                colors.push(0, 0, 0, 0);
+            }
+
+            settings.push(effect.settings[0], effect.settings[1], effect.settings[2], effect.settings[3]);
+        }
+
+        shader.setUniform("u_effectTypes", types);
+        shader.setUniform("u_effectCenters", centers);
+        shader.setUniform("u_effectDimensions", dimensions);
+        shader.setUniform("u_effectColors", colors);
+        shader.setUniform("u_effectSettings", settings);
+
+        s.shader(shader);
+        s.rect(0-s.width/2, 0-s.height/2, s.width, s.height);
+
+        if (s.frameRate() !== 0) {
+            t += 1/s.frameRate() * globalTimeMultiplier;
+            if (frameRateVisible && Date.now() - lastFrameRateEmission > 1000) {
+                console.log(s.frameRate().toFixed(0));
+                lastFrameRateEmission = Date.now();
+            }
+        }
+    };
+
+    s.keyPressed = () => {
+        if (s.key === " ") {
+            frameRateVisible = !frameRateVisible;
+        }
+        if (s.keyCode === s.ENTER) {
+            if (s.isLooping()) {
+                s.noLoop();
+            } else {
+                s.loop();
+            }
+        }
+    };
+
+
+};
+
+new P5(sketch);
