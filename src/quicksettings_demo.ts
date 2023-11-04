c6fb6a4550a1f325e9cc4cc24f16ee0b96922a01
Kieran Sagar Parikh <kieran@parikh.ca>
2023-11-03 22:48:42 -0400

    quicksettings demo based on untitled 16

--- src/quicksettings_demo.ts
+++ src/quicksettings_demo.ts
@@ -7,58 +7,75 @@ import QuickSettings from "quicksettings-iterator";
 import {setAspectRatioStr} from "./helpers/aspect_ratio";
 import {IsoLine} from "./helpers/isometric";
 
-// Description: 
+// Description: Demo for Quicksettings-iterator, using a modified version of untitled_16.
 // Date: 11/1/23 22:09:35Z
 
 const q = {
-    param: 3,
-    theta: 50,
+    numLines: 75,
+    spacing: 10,
+    lineLen: 1000,
+    ptsPerLine: 10,
+    disturbance: 100,
+    color1: "#018f14",
+    color2: "#002afd",
+    zoom: 100,
+    weight: 1,
+    theta: 30,
 };
 const settings = QuickSettings.create(10, 10, "settings");
-settings.bindRange("param", 0, 50, q.param, 1, q);
+// settings.bindNumber("numLines", 0, 1000, q.numLines, 1,  q);
+// settings.bindRange("spacing", 0, 100, q.spacing, 0.5,  q);
+// settings.bindRange("lineLen", 0, 2000, q.lineLen, 1,  q);
+settings.bindRange("ptsPerLine", 0, 100, q.ptsPerLine, 1,  q);
+settings.bindRange("disturbance", 0, 300, q.disturbance, 5,  q);
+settings.bindRange("zoom", 0, 300, q.zoom, 5,  q);
+// settings.bindRange("weight", 0, 10, q.weight, 0.1, q);
+// settings.bindRange("theta", 0, 180, q.theta, 1, q);
 
 init(P5);
 const sketch = (s: p5SVG) => {
     s.setup = () => {
         s.createCanvas(s.windowWidth, s.windowHeight);
-        settings.addText("Aspect Ratio", "11x14", (aspect: string) => {
-            setAspectRatioStr(s, aspect);
-        });
+        // settings.addText("Aspect Ratio", "11x14", (aspect: string) => {
+        //     setAspectRatioStr(s, aspect);
+        // });
         s.frameRate(10);
         s.angleMode(s.DEGREES);
     };
 
     s.draw = () => {
-        s.translate(s.width/2, s.height/2);
-        s.background(255);
+        // s.translate(s.width/2, s.height/2);
         settings.iterationStep();
-        s.stroke(0);
+        s.background(255);
+        s.translate(s.width/2 - q.numLines*q.spacing/2, s.height/2 - q.lineLen/2);
         s.noFill();
-        for (let i = 0; i <= 10; i++) {
-            // IsoLine(s, [
-            //     {x: -i*10, y: 0, z: -i*10},
-            //     {x: -100, y: 0, z: -i*10},
-            //     {x: -100, y: -100, z: -i*10},
-            // ], q.theta);
-            // IsoLine(s, [
-            //     {x: -i*10, y: 0, z: -i*10},
-            //     {x: -i*10, y: 0, z: -100},
-            //     {x: -i*10, y: -100, z: -100},
-            // ], q.theta);
-            IsoLine(s, [
-                {x: -100, y: -100, z: -i*10},
-                {x: -100, y: 0, z: -i*10},
-                {x: -i*10, y: 0, z: -100},
-                {x: -i*10, y: -100, z: -100},
-            ], q.theta);
+        s.stroke(q.color1);
+        s.strokeWeight(q.weight);
+        for (let i = 0; i < q.numLines; i++) {
+            s.beginShape();
+            const x = i*q.spacing;
+            s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, 0), 0);
+            for (let j = 0; j <= q.ptsPerLine; j++) {
+                const y = j*q.lineLen/q.ptsPerLine;
+                s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, y/q.zoom), y);
+            }
+            s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, 0), q.lineLen);
+            s.endShape();
         }
 
-
-        // IsoLine(s, [
-        //     {x: -10, y: 0, z: 10},
-        //     {x: -100, y: 0, z: 10},
-        //     {x: -100, y: -100, z: 10},
-        // ], q.theta);
+        s.translate(q.spacing/2,0);
+        s.stroke(q.color2);
+        for (let i = 0; i < q.numLines; i++) {
+            s.beginShape();
+            const x = i*q.spacing;
+            s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, 0), 0);
+            for (let j = 0; j <= q.ptsPerLine; j++) {
+                const y = j*q.lineLen/q.ptsPerLine;
+                s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, y/q.zoom), y);
+            }
+            s.curveVertex(x + q.disturbance*s.noise(x/q.zoom, 0), q.lineLen);
+            s.endShape();
+        }
     };
 
     s.mouseClicked = () => {
