58f59551dd6c0788b5adfc0f4e1d47575984e084
Kieran Parikh <kieran@parikh.ca>
2024-12-02 20:59:25 -0500

    game of life

--- src/hexagon.ts
+++ src/hexagon.ts
@@ -18,10 +18,17 @@ const BRANCHING = "branching";
 const params = {
     layers: 5,
     radius: 50,
+    underpopulation: 2,
+    overpopulation: 3,
+    reproduction: 3,
 };
 const {pane, uiWidth} = initPaneAtLeft(1.1, {title: "Circles"});
 pane.addBinding(params, "layers");
 pane.addBinding(params, "radius");
+pane.addBinding(params, "underpopulation", {min: 0, max: 10, step: 1});
+pane.addBinding(params, "overpopulation", {min: 0, max: 10, step: 1});
+pane.addBinding(params, "reproduction", {min: 0, max: 10, step: 1});
+
 
 interface LightEffect {
     // intensity: number;
@@ -32,10 +39,17 @@ interface LightEffect {
 }
 
 const activeHexagons = new Set<string>;
-let state = new Map<string, LightEffect[]>();
+let CAState = new Set<string>;
+CAState.add("0 0 0");
+CAState.add("0 0 1");
+CAState.add("0 0 2");
+CAState.add("0 0 3");
+CAState.add("0 0 4");
+CAState.add("0 0 5");
+const state = new Map<string, LightEffect[]>();
 // state.set("0 0 0", [{pattern: BRANCHING}]);
 // state.set("-1 0 2", [{pattern: PERIMETER}]);
-state.set("0 0 0", [{pattern: RANDOM}]);
+// state.set("0 0 0", [{pattern: RANDOM}]);
 // state.set("0 0 0", [{pattern: RANDOM}]);
 // state.set("0 0 0", [{pattern: RANDOM}]);
 // state.set("0 0 0", [{pattern: RANDOM}]);
@@ -49,6 +63,12 @@ state.set("0 0 0", [{pattern: RANDOM}]);
 // state.set("2 2 2", [{pattern: PERIMETER}]);
 // state.set("3 4 2", [{pattern: PERIMETER}]);
 activeHexagons.add("0 0");
+activeHexagons.add("1 0");
+activeHexagons.add("-1 0");
+activeHexagons.add("0 1");
+activeHexagons.add("0 -1");
+activeHexagons.add("1 1");
+activeHexagons.add("-1 -1");
 
 init(P5);
 const sketch = (s: p5SVG) => {
@@ -57,7 +77,7 @@ const sketch = (s: p5SVG) => {
         setBackground(s.color("#999999"));
         setAspectRatioStr(s, "1x1", maxWidth(800, uiWidth), maxHeight());
         defaultPaneHelpers(pane, s, sketch, maxWidth(800, uiWidth));
-        s.frameRate(10);
+        s.frameRate(5);
     };
 
     s.draw = () => {
@@ -100,6 +120,13 @@ function decodeStateKey(key: string) {
     return {q, r, edge};
 }
 
+function decodeActiveHexKey(key: string) {
+    const v = key.split(" ");
+    const q = parseInt(v[0]);
+    const r = parseInt(v[1]);
+    return {q, r};
+}
+
 function encodeKey(q: number, r: number, edge?: number) {
     let key = q.toString() + " "  + r.toString();
     if (edge !== undefined) {
@@ -116,27 +143,69 @@ function mapAppend(map: Map<string, LightEffect[]>, key: string, value: LightEff
 }
 
 function advanceHexState() {
-    const newState = new Map<string, LightEffect[]>();
-    state.forEach((effects, key) => {
-        const {q, r, edge} = decodeStateKey(key);
+    // const newState = new Map<string, LightEffect[]>();
+    // state.forEach((effects, key) => {
+    //     const {q, r, edge} = decodeStateKey(key);
+    //     for (const effect of effects) {
+    //         let s;
+    //         if (effect.pattern === PERIMETER) {
+    //             s = perimeterEffect(q, r, edge, effect);
+    //         } else if (effect.pattern === RANDOM) {
+    //             s = randomEffect();
+    //         } else if (effect.pattern === NEXTADJ) {
+    //             s = nextAdjEffect(q, r, edge, effect);
+    //         } else if (effect.pattern === BRANCHING) {
+    //             s = branchingEffect(q, r, edge, effect);
+    //         }
+    //         for (const entry of s) {
+    //             mapAppend(newState, encodeKey(entry.newQ, entry.newR, entry.newEdge), effect);
+    //         }
+    //     }
+    // });
+    // state = newState;
 
-        for (const effect of effects) {
-            let s;
-            if (effect.pattern === PERIMETER) {
-                s = perimeterEffect(q, r, edge, effect);
-            } else if (effect.pattern === RANDOM) {
-                s = randomEffect();
-            } else if (effect.pattern === NEXTADJ) {
-                s = nextAdjEffect(q, r, edge, effect);
-            } else if (effect.pattern === BRANCHING) {
-                s = branchingEffect(q, r, edge, effect);
+    const newCAState = new Set<string>;
+
+    activeHexagons.forEach((key) => {
+        const {q, r} = decodeActiveHexKey(key);
+        for (let edge = 0; edge < 6; edge++) {
+            // count the number of live edges (should hexagons that aren't active count as live?)
+            let neighbors = 0;
+            const neighborEdges = determineNeighborEdges(q, r, edge);
+            for (const neighborEdge of neighborEdges) {
+                if (CAState.has(encodeKey(neighborEdge.q, neighborEdge.r, neighborEdge.edge))) {
+                    neighbors += 1;
+                }
             }
-            for (const entry of s) {
-                mapAppend(newState, encodeKey(entry.newQ, entry.newR, entry.newEdge), effect);
+            const live = CAState.has(encodeKey(q, r, edge));
+
+            if (live && (neighbors < params.underpopulation || neighbors > params.overpopulation)) {
+                // cell dies
+                continue;
+            } else if (live || neighbors == params.reproduction) {
+                newCAState.add(encodeKey(q, r, edge));
             }
         }
     });
-    state = newState;
+
+    CAState = newCAState;
+}
+
+function determineNeighborEdges(q: number, r: number, edge: number) {
+    const out = [
+        {q, r, edge: (edge + 1) % 6},
+        {q, r, edge: (edge - 1) % 6},
+        {q, r, edge: (edge + 2) % 6},
+        {q, r, edge: (edge - 2) % 6},
+        {q, r, edge: (edge - 3) % 6},
+    ];
+    const adj = edgeIsAdjacentToLiveHexagon(q, r, edge);
+    if (adj !== undefined) {
+        out.push({q: adj.newQ, r: adj.newR, edge: adj.newEdge},
+            {q: adj.newQ, r: adj.newR, edge: (adj.newEdge + 1) % 6},
+            {q: adj.newQ, r: adj.newR, edge: (adj.newEdge - 1) % 6});
+    }
+    return out;
 }
 
 function randomEffect() {
@@ -308,11 +377,11 @@ function drawHexagon(s: p5SVG, q: number, r: number) {
                 // 5 - N
                 // 6 - NE
 
-                if (state.has(encodeKey(q, r, i - 1))) {
+                if (state.has(encodeKey(q, r, i - 1)) || CAState.has(encodeKey(q, r, i - 1))) {
                     s.stroke(100);
                 } else {
-                    // s.stroke(230);
-                    s.stroke(255);
+                    s.stroke(230);
+                    // s.stroke(255);
                 }
 
                 //
